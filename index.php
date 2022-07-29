8fc5b957d723aa21fc6c01a0eef14b6f99ebabb2
TuanNH <tuannh85006@co-well.vn>
2022-07-29 18:02:38 +0700

    add .htaccess

--- index.php
+++ index.php
@@ -3,7 +3,15 @@ require('Config/Database.php');
 $db = new Database;
 $db->connect();
 
-/*Xử lý các request*/
+echo $_GET['controller'];
+
+die();
+
+if (isset($_GET['controller'])) {
+    require 'Route/web.php'; /*xử lý các request trong Route/web.php*/
+} else {
+    require 'Views/index.php'; /*require giao diện trang chủ*/
+}
 
 
 $db->closeDatabase();
