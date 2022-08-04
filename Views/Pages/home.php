22d90e4a820209ad552e8cd28467b8174ee3887e
phamthimaituyet <phamthimaituyet2000@gmail.com>
2022-08-04 09:50:16 +0700

    css

--- Views/Pages/home.php
+++ Views/Pages/home.php
@@ -6,21 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
-    <link href="./public/css/main.css" rel="stylesheet">
+    <link href="./public/css/home.css" rel="stylesheet">
 
 </head>
 
 <body class="p-3">
 
-<form action="/php/home/create">
-    <div class="form-group">
-        <input type="text" class="form-control" id="inputUser" name="create-note" placeholder="Create new note"/>
-        <input type="submit" value="Create" class="btn btn-primary mt-1"/>
-    </div>
-</form>
+    <div id="app">
+        <h1>To do notes</h1>
+        <form action="/php/home/create">
+            <div class="form-group">
+                <input type="text" class="form-control" id="inputUser" name="create-note"
+                    placeholder="Create new note" />
+                <input type="submit" value="Create" class="btn_add" />
+            </div>
+        </form>
 
-<h3>Your Note</h3>
-<?php
+        <h3>Your Note</h3>
+        <?php
 
 if (!empty($notes)) {
     echo "<form>";
@@ -31,23 +34,25 @@ if (!empty($notes)) {
                     <p class='fst-italic'>
                     " . $note['content'] . "
                     </p>
-                <button
-                class='btn btn-danger'
-                type='submit' 
-                formaction='/php/home/delete' 
-                name='delete'
-                value='" . $note['id'] . "'>
-                Delete
-                </button>
-                
-                <button 
-                class='btn btn-warning'
-                type='submit' 
-                formaction='/php/home/toEdit'
-                value='" . $note['id'] . "'
-                name='note_id'>
-                Edit
-                </button>
+                    <div>
+                    <button
+                    class='btn btn-danger'
+                    type='submit' 
+                    formaction='/php/home/delete' 
+                    name='delete'
+                    value='" . $note['id'] . "'>
+                    Delete
+                    </button>
+                    
+                    <button 
+                    class='btn btn-warning'
+                    type='submit' 
+                    formaction='/php/home/toEdit'
+                    value='" . $note['id'] . "'
+                    name='note_id'>
+                    Edit
+                    </button>
+                    </div>
                 </li>";
     }
     echo "</ul>";
@@ -57,6 +62,9 @@ if (!empty($notes)) {
 }
 
 ?>
+
+    </div>
+
 
 
 </body>
